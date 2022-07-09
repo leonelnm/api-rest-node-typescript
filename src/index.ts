fe2eb7ab0d65fbe0995e6c2a6539a0ddaffda892
Erik Navarrete <elerickmontenegro@gmail.com>
2022-07-09 18:45:13 +0200

    Configure linter

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ app.get('/ping', (_req, res) => {
 // Add routes
 app.use('/api/diaries', diaryRouter)
 
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
+  console.log(`Server running on port ${PORT}...`)
 })
--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,10 +1,9 @@
-import express from "express"
+import express from 'express'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
   res.send('Fetching all entry diaries')
-
 })
 
 router.post('/', (_req, res) => {
