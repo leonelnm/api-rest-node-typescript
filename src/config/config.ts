24ce2e6ee3fb62e9cb1582527139013fecf5aae2
Erik Navarrete <elerickmontenegro@gmail.com>
2022-07-13 22:23:47 +0200

    configuración inicial para DB con postgresql

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,12 +12,18 @@ const envVarsSchema = Joi.object().keys({
   JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
   JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
     .default(10)
-    .description('minutes after which reset password token expires')
+    .description('minutes after which reset password token expires'),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().default(5432), // default puerto postgresql
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_USERNAME: Joi.string().required(),
+  POSTGRES_LOGGING: Joi.boolean().default(false),
+  POSTGRES_DIALECT: Joi.boolean().default('postgres')
 }).unknown()
 
 const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env)
 
-if (error != null) {
+if (error !== null && error !== undefined) {
   throw new Error(`Config validation error: ${error.message}`)
 }
 
@@ -30,5 +36,14 @@ export default {
     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
     resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
     verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
+  },
+  dbConnection: {
+    host: envVars.POSTGRES_HOST,
+    port: envVars.POSTGRES_PORT,
+    database: envVars.POSTGRES_DB,
+    username: envVars.POSTGRES_USERNAME,
+    password: envVars.POSTGRES_PASSWORD,
+    dialect: envVars.POSTGRES_DIALECT,
+    logging: envVars.POSTGRES_LOGGING
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata'
+
 import app from './app'
 import config from './config/config'
 import Logger from './config/logger'
